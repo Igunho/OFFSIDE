17fd9043e2e817f4019b2f224beeec38b89736e1
Igor Araújo <igorsantos.7262@gmail.com>
2025-06-18 12:11:18 -0300

    Add files via upload

---
+++ pscript.js
@@ -0,0 +1,26 @@
+let iterations = {
+    novidades: 0
+}
+
+$(document).ready(function() {
+    $("#next").click(function() {
+        const hidden = $("#next").siblings(".card:hidden")
+
+        console.log(hidden)
+        if(iterations.novidades < 4) {
+            $(hidden[iterations.novidades]).show()
+            $("#next").siblings(".card:visible:first").hide()
+            iterations.novidades++
+        }
+    })
+    $("#previous").click(function() {
+        const hidden = $("#next").siblings(".card:hidden")
+
+        console.log(hidden)
+        if(iterations.novidades > 0) {
+            $(hidden[iterations.novidades-1]).show()
+            $("#next").siblings(".card:visible:last").hide()
+            iterations.novidades--
+        }
+    })
+})
